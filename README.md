8ff2fb9eb7a8477fb0b159fd75f0b38446fc27c3
Ben Picolo <bpicolo@umich.edu>
2014-05-22 15:33:15 -0700

    Find longest common path match of all open parent folderts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Adds a "Copy Relative Path" command to the sublime right-click context menu,
-which copies your file path relative to the root directory of the currently opened
-project.
+# Copy Relative Path
+Adds a "Copy Relative Path" command to the Sublime right-click context menu,
+which copies the file path of the currently opened file relative
+to the folder you have open with the greatest path match.
+
+## Adding a Hotkey
+     {"keys": ["super+i"], "command": "copy_relative_path"},
--- copy_relative_path.py
+++ copy_relative_path.py
@@ -1,9 +1,20 @@
 import sublime, sublime_plugin
+from os.path import relpath
 
 class CopyRelativePathCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if len(self.view.file_name()) > 0:
-            sublime.set_clipboard( sublime.active_window().active_view().file_name().replace( sublime.active_window().folders()[0] + '/', '' ))
+        filename = self.view.file_name()
+        if len(filename) > 0:
+            # Copy shortest relpath for file compared to open folders
+            sublime.set_clipboard(
+                min(
+                    (
+                        relpath(filename, folder)
+                        for folder in sublime.active_window().folders()
+                    ),
+                    key=len,
+                )
+            )
             sublime.status_message("Copied relative path")
 
     def is_enabled(self):
